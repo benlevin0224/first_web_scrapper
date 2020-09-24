54a43894888ea97aaed2b11f723170734699704f
benlevin0224 <71128482+benlevin0224@users.noreply.github.com>
2020-09-23 23:03:10 -0400

    Update web_scrap.py

--- web_scrap.py
+++ web_scrap.py
@@ -1,28 +1,31 @@
 import urllib.request, urllib.parse, urllib.error
+from urllib.request import Request, urlopen
 from bs4 import BeautifulSoup
+#https://stackoverflow.com/questions/16627227/http-error-403-in-python-3-web-scraping
+class AppURLopener(urllib.request.FancyURLopener):
+    version = "Mozilla/5.0"
+opener = AppURLopener()
+response = opener.open('https://www.cardkingdom.com/catalog/search?search=header&ac=1&filter%5Bname%5D=Mirari%27s%20Wake')
+soup = BeautifulSoup(response, 'html.parser')
 
-url = 'https://www.newegg.com/p/pl?d=gaming+monitors&N=600554755%20100898493'
-html = urllib.request.urlopen(url).read()
-soup = BeautifulSoup(html, 'html.parser')
-containers = soup.findAll("div",{"class":"item-container"})
-container = containers[0]
-
-filename = "products.csv"
+condition = {0:"NM",1:"EX",2:"VG",3:"G"}
+count = 0
+filename = "mtg.csv"
 f = open(filename, "w")
-headers = "brand, product_name, shipping\n"
+headers = "card, edition, quanity, condition, price\n"
 f.write(headers)
-
-for container in containers:
-    brand = container.div.div.a.img["alt"]
-
-    title_container = container.findAll("a",{"class":"item-title"})
-    product_name = title_container[0].text
-
-    shipping_container = container.findAll("li",{"class":"price-ship"})
-    shipping = shipping_container[0].text.strip()
-
-    print("brand: " + brand)
-    print("product name: " + product_name)
-    print("shipping: " + shipping)
-    f.write(brand + "," +product_name.replace(","," ") + "," + shipping + "\n")
+item_wrapper = soup.findAll("div",{"class":"itemContentWrapper"})
+cart_wrapper = soup.findAll("div",{"class":"amtAndPrice"})
+name = []
+edition = []
+for item_wrap in item_wrapper:
+    name.append(item_wrap.td.span.text.strip())
+    edition.append(item_wrap.div.text.strip())
+name_index = 0
+for cart_wrap in cart_wrapper:
+    if count == 4:
+        count = 0
+        name_index += 1
+    f.write(name[name_index]+","+edition[name_index]+","+cart_wrap.text.strip() +"," + condition[count] +","+"\n")
+    count += 1
 f.close()
